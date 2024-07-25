a2a871a461b8a81dc13b4d86e46d15bede8edaae
hahan <hanvietha141@gmail.com>
2024-07-25 22:50:49 +0700

    (+) generate new component

---
+++ src/builder-registry.ts
@@ -0,0 +1,42 @@
+"use client";
+import { builder, Builder } from "@builder.io/react";
+import Counter from "./components/Counter/Counter";
+import ImageWithText from "./components/ImageWithText/ImageWithText";
+import MyComponent from "./components/MyComponent/MyComponent";
+
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+Builder.registerComponent(Counter, {
+  name: "Counter",
+  inputs: [
+    {
+      name: "initialCount",
+      type: "number",
+    },
+  ],
+});
+
+Builder.registerComponent(ImageWithText, {
+  name: "ImageWithText",
+  inputs: [
+    {
+      name: "description",
+      type: "string",
+      required: true,
+    },
+    {
+      name: "imageSrc",
+      type: "string",
+      required: true,
+    },
+    {
+      name: "title",
+      type: "string",
+      required: true,
+    },
+  ],
+});
+
+Builder.registerComponent(MyComponent, {
+  name: "MyComponent",
+});
